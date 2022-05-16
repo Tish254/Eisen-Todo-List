ecdd04391209d71ae2b9cf45b7aa47daeebb31fa
Tish254 <tiegomseeraoscar295@gmail.com>
2022-05-16 15:44:58 +0300

    heade minor changes

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,14 +5,14 @@ const Title = ({title, menuClicked}) => {
     <div className="flex justify-between">
 
         <div className="self-start justify-self-start">
-          <h1 className="relative text-3xl font-bold before:block before:content[''] before:-mt-2.5 before:h-1.5 before:absolute before:left-0 before:w-12 md:before:w-10 before:rounded before:bg-violet-600">
+          <h1 className="relative text-2xl md:text-3xl font-bold before:block before:content[''] before:-mt-2.5 before:h-1.5 before:absolute before:left-0 before:w-12 md:before:w-10 before:rounded before:bg-violet-600">
               {title}
           </h1>
           <p className="font-extralight text-sm">
               {moment().format('MMMM Do YYYY')}
           </p>
         </div>
-        <div className="text-3xl md:ml-auto font-bold text-left mb-2">
+        <div className=" text-2xl md:text-3xl md:ml-auto font-bold text-left mb-2">
             <h1>
               <strong>
                   Eisen
